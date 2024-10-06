54a10126130a02376441d1dfd053836f1bb988b5
delay001 <artyom2037@gmail.com>
2024-10-06 22:22:41 +0300

    Add files via upload

---
+++ homework/work_4.1.py
@@ -0,0 +1,9 @@
+def unique(sentence):
+    words = sentence.split()
+    uniqueW = set(words)
+    sortedW = sorted(uniqueW)
+    return sortedW
+
+sentence = "apple banana apple orange banana kiwi"
+result = unique(sentence)
+print(result)
